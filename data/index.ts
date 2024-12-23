5f7ae8777a0e605c567a5dde1bdccad629756305
Axeolotl <a.d.l.c.rubio@gmail.com>
2024-12-22 23:54:39 -0800

    p

--- data/index.ts
+++ data/index.ts
@@ -12,7 +12,7 @@ export const navItems = [
       className: "lg:col-span-3 md:col-span-6 space-y-4 md:row-span-4 lg:min-h-[60vh]",
       imgClassName: "w-full h-full",
       titleClassName: "justify-end",
-      img: "/spc.svg",
+      img: "/me.png",
       spareImg: "",
     },
     {
@@ -48,7 +48,7 @@ export const navItems = [
   
     {
       id: 5,
-      title: "Backend or Frontend? Why not both! I can flex if needed ",
+      title: "Backend or Frontend? Why not both! I can flex if needed. One or the other,or BOTH, I can do it. ",
       description: "",
       className: "md:col-span-3 md:row-span-2 space-y-4",
       imgClassName: "absolute right-0 bottom-0 md:w-96 w-60",
